ba819f73e10ebdbd65c2c7e0a67b22a15ebe82a9
Rifki Rahadian <rrahadian94@gmail.com>
2024-04-17 09:01:41 +0700

    chore: init

---
+++ src/app/signup/page.js
@@ -0,0 +1,110 @@
+'use client';
+
+import Link from "next/link";
+import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function Signup() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCzwfvF71ii36Qpr0C5cDaAnEmdA2Fk_G4",
+    authDomain: "nest-auth-firebase-2825a.firebaseapp.com",
+    projectId: "nest-auth-firebase-2825a",
+    storageBucket: "nest-auth-firebase-2825a.appspot.com",
+    messagingSenderId: "808372221064",
+    appId: "1:808372221064:web:2fa3b6c6df709cfd3c9b3d"
+  };
+  
+  initializeApp(firebaseConfig);
+  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, password });
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log({ user: user.reloadUserInfo });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        switch (errorCode) {
+          case 'auth/email-already-in-use':
+            alert('Email already in use');
+            break;
+          
+          case 'auth/invalid-email':
+            alert('Invalid Email');
+            break;
+        
+          default:
+            alert(errorMessage);
+            break;
+        }
+      });
+  };
+
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-bold text-center text-gray-700">Register</h1>
+        <form className="mt-6" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Username
+            </label>
+            <input
+              required
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          
+          <div className="mt-2">
+            <button type="submit" className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Register
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-4 text-sm text-center text-gray-700">
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className="font-medium text-blue-600 hover:underline"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
